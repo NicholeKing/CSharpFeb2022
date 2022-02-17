87cbd0379e7f9b1dbd11016a63064038df6dbac2
NicholeKing <nichole.king94@hotmail.com>
2022-02-17 12:39:34 -0500

    LogReg and DLL

---
+++ Day09/LogReg/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LogReg.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace LogReg.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext _context;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, MyContext dbContext)
+        {
+            _logger = logger;
+            _context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(User newUser)
+        {
+            if(ModelState.IsValid)
+            {
+                if(_context.Users.Any(u => u.Email == newUser.Email))
+                {
+                    // Manually add a ModelState error to the Email field, with provided
+                    // error message
+                    ModelState.AddModelError("Email", "Email already in use!");
+                    // You may consider returning to the View at this point
+                    return View("Index");
+                }
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+                _context.Users.Add(newUser);
+                _context.SaveChanges();
+                return RedirectToAction("Success");
+            } else {
+                return View("Index");
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LogUser logUser)
+        {
+            if(ModelState.IsValid)
+            {
+                User userInDb = _context.Users.FirstOrDefault(s => s.Email == logUser.LEmail);
+                if(userInDb == null)
+                {
+                    // If it's null then they are NOT in the database
+                    ModelState.AddModelError("LEmail", "Invalid login attempt");
+                    return View("Index");
+                }
+                // If we are the correct email it's time to check the password
+                // First we make another instance of the password hasher
+                PasswordHasher<LogUser> Hasher = new PasswordHasher<LogUser>();
+                PasswordVerificationResult result = Hasher.VerifyHashedPassword(logUser, userInDb.Password, logUser.LPassword);
+                // When it's done it will return either a 1 or a 0
+                // 1 means you were successful
+                // 0 means you were not successful
+                if(result == 0)
+                {
+                    ModelState.AddModelError("LEmail", "Invalid login attempt");
+                    return View("Index");
+                }
+                return RedirectToAction("Success");
+            } else {
+                return View("Index");
+            }
+        }
+
+        [HttpGet("Success")]
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
